fe4d134c7eb381b82176d6591f09770daaeb016b
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-02-10 02:45:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ app.use(express.json())
 // Backend
 const { tinyUrl } = require("./backend/shortURL")
 
-app.set("json spaces", 4)
+app.set("json spaces", 4);
 
 app.get("/api/tinyUrl", async (req, res) => {
     const { url } = req.query
     if (!url) {
-        return res.status(400).send(JSON.stringify({
-            message: "Berikan saya URL!"
-        }, null, 4))
+        return res.status(400).send(message: "Berikan saya URL!")
     }
     try {
         const result = await tinyUrl(url)
-        res.send(JSON.stringify(result, null, 4))
+        res.send(result)
     } catch (error) {
-        res.send(JSON.stringify(result, null, 4))
+        res.send(result)
     }
 })
 
