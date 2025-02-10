49b2ffe06d8f6284affdf704364979fca1517d58
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-02-10 18:22:29 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Backend
 const { tinyUrl } = require("./backend/shortURL");
-const { ytsearch } = require("./backend/search");
-
 app.set("json spaces", 4);
 
 // Endpoint API
@@ -22,46 +20,12 @@ app.get("/api/tinyUrl", async (req, res) => {
     }
     try {
         const result = await tinyUrl(url);
-        res.json({
-            result
-        });
+        res.json(result);
     } catch (error) {
-        res.status(500).json({
-            error
-        });
+        res.status(500).json(error);
     }
 });
 
-app.get("/api/ytsearch", async (req, res) => {
-    const { query } = req.query;
-
-    if (!query) {
-        return res.status(400).json({ 
-            status: 400, 
-            message: "Berikan saya JUDUL!" 
-        });
-    }
-
-    try {
-        const result = await ytsearch(query);
-
-        if (result.status === 404) {
-            return res.status(404).json({
-                result
-            });
-        }
-
-        res.json({
-            result
-        });
-    } catch (error) {
-        res.status(500).json({
-            error
-        });
-    }
-});
-
-// Jalankan server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Running in port: ${PORT}`);
