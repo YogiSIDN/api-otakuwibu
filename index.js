d41694ff26f6deb96ba7cec72c6fcff677bbc319
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-02-11 04:01:40 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 // Backend
 const { tinyUrl } = require("./backend/shortURL")
-const { yts } = require("./backend/search")
+const { sps, yts } = require("./backend/search")
 app.set("json spaces", 4)
 
 app.get("/api/sfw/loli", async (req, res) => {
@@ -71,6 +71,30 @@ app.get("/api/sfw/waifu", async (req, res) => {
         });
     }
 });
+
+app.get("/api/spotifySearch", async (req, res) => {
+       const { q } = req.query;
+       if (!q) {
+           return res.status(400).json({ 
+               status: 400,
+               dev: "@mysu_019",
+               message: "Terjadi kesalahan." 
+           });
+       }
+       try {
+           const result = await sps(q);
+           if (result.status !== 200) {
+               return res.status(result.status).json(result);
+           }
+           res.json(result);
+       } catch (error) {
+           res.status(500).json({
+               status: 500,
+               dev: "@mysu_019",
+               message: "Terjadi kesalahan."
+           });
+       }
+})
 
 app.get("/api/yts", async (req, res) => {
        const { q } = req.query;
