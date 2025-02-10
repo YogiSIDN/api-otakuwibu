48efb4f86c68f45a0d79fada0d75f8eda75d7bf5
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-02-10 19:31:12 +0700

    Add files via upload

--- backend/search.js
+++ backend/search.js
@@ -1,30 +1,38 @@
 const ytSearch = require("youtube-search-api");
 
 async function yts(query) {
-    try {
-        const res = await ytSearch.GetListByKeyword(query, false, 10); 
-        const videos = res.items.map(video => ({
-            id: video.id,
-            title: video.title,
-            duration: video.length.simpleText || "N/A",
-            channel: video.channelTitle,
-            thumbnail: video.thumbnail.thumbnails.pop().url,
-            link: `https://www.youtube.com/watch?v=${video.id}`
-        }));
+       try {
+           const res = await ytSearch.GetListByKeyword(query, false, 10);
+           if (!res.items || res.items.length === 0) {
+               return {
+                   status: 404,
+                   dev: "@mysu_019",
+                   message: "Tidak ada hasil yang ditemukan."
+               };
+           }
+           const videos = res.items.map(video => ({
+               id: video.id,
+               title: video.title,
+               duration: video.length.simpleText || "N/A",
+               channel: video.channelTitle,
+               thumbnail: video.thumbnail.thumbnails.pop().url,
+               link: `https://www.youtube.com/watch?v=${video.id}`
+           }));
 
-        return {
-            status: 200,
-            dev: "@mysu_019",
-            data: videos
-        };
-    } catch (error) {
-        return {
-            status: 500,
-            dev: "@mysu_019",
-            message: "Terjadi kesalahan.",
-            error: error.message
-        };
-    }
-}
+           return {
+               status: 200,
+               dev: "@mysu_019",
+               data: videos
+           };
+       } catch (error) {
+           return {
+               status: 500,
+               dev: "@mysu_019",
+               message: "Terjadi kesalahan.",
+               error: error.message
+           };
+       }
+   }
+
 
 module.exports = { yts }
