6e869186fdbac57ee94f6460262a4b89541422ff
Mohammad Yogi Sugalih <96093606+YogiSIDN@users.noreply.github.com>
2025-02-10 09:35:51 +0700

    Initial commit

--- backend/shortURL.js
+++ backend/shortURL.js
@@ -1,4 +1,3 @@
-const axios = require("axios")
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 // Generate Short URL
@@ -25,37 +24,6 @@ async function tinyUrl(links) {
     });
 }
 
-async function akariUrl(links) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await axios.post("https://api.waa.ai/v2/links", {
-                url: links
-            }, {
-                headers: {
-                    'Authorization': `Bearer 767c4f1c12619aa5226d28f109fd032532d2c637`,
-                    'Content-Type': 'application/json'
-                }
-            })
-            const shortUrl = response.data.data.link
-            resolve({
-                status: 200,
-                dev: "@mysu_019",
-                data: {
-                    baseUrl: links,
-                    shortUrl: shortUrl
-                }
-            })
-        } catch (error) {
-            reject({
-                status: 500,
-                dev: "@mysu_019",
-                message: "Tejadi kesalahan saat membuat short Url."
-            })
-        }
-    })
-}
-
 module.exports = {
-    tinyUrl,
-    akariUrl
+    tinyUrl
 }
